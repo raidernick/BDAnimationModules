9a99aeb1d41fcf4a420e75787d4fbc3098d4e982
raidernick <justanotherdude666@gmail.com>
2017-03-29 17:42:12 -0400

    recompile for ksp 1.2.9pre

--- BDAnimationModules/AssemblyInfo.cs
+++ BDAnimationModules/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BDAnimationModules.Properties")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.6.4.4")]
-[assembly: AssemblyFileVersion("0.6.4.4")]
+[assembly: AssemblyVersion("0.6.4.5")]
+[assembly: AssemblyFileVersion("0.6.4.5")]
