5c7f4e5df0690dc8a25e0ec3b78854ace421ca39
raidernick <raidernick@users.noreply.github.com>
2017-07-14 20:53:59 -0400

    update to include rnmodules

--- BDAnimationModules/AssemblyInfo.cs
+++ BDAnimationModules/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.6.4.5")]
-[assembly: AssemblyFileVersion("0.6.4.5")]
+[assembly: AssemblyVersion("0.6.4.6")]
+[assembly: AssemblyFileVersion("0.6.4.6")]
