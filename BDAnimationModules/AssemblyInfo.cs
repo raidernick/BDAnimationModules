cc496c4ae110401c044c8d72c58aaf3391eedbf4
raidernick <raidernick@users.noreply.github.com>
2018-03-28 19:06:25 -0400

    update for ksp 1.4.2

--- BDAnimationModules/AssemblyInfo.cs
+++ BDAnimationModules/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.6.5.2")]
-[assembly: AssemblyFileVersion("0.6.5.2")]
+[assembly: AssemblyVersion("0.6.5.3")]
+[assembly: AssemblyFileVersion("0.6.5.3")]
--- rnmodules/Properties/AssemblyInfo.cs
+++ rnmodules/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.5.5")]
+[assembly: AssemblyVersion("1.1.5.6")]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyFileVersion("1.1.5.5")]
+[assembly: AssemblyFileVersion("1.1.5.6")]
 [assembly: AssemblyProduct("RNModules")]
 [assembly: AssemblyTitle("RNModules")]
 [assembly: AssemblyTrademark("")]
