6107b4dd8092407a8f1b680b740fe738d195841d
raidernick <raidernick@users.noreply.github.com>
2018-12-23 01:34:38 -0500

    update for ksp 1.6

--- rnmodules/Properties/AssemblyInfo.cs
+++ rnmodules/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.5.8")]
+[assembly: AssemblyVersion("1.1.5.9")]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyFileVersion("1.1.5.8")]
+[assembly: AssemblyFileVersion("1.1.5.9")]
 [assembly: AssemblyProduct("RNModules")]
 [assembly: AssemblyTitle("RNModules")]
 [assembly: AssemblyTrademark("")]
