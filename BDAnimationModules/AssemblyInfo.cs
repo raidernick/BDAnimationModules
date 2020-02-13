58f3b9d204ca6225bb6cb0ca7f57e10abe4fd6f7
raidernick <raidernick@users.noreply.github.com>
2020-02-13 17:58:38 -0500

    update for ksp 1.9.x

--- BDAnimationModules/AssemblyInfo.cs
+++ BDAnimationModules/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BDAnimationModules.Properties")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.6.5.8")]
-[assembly: AssemblyFileVersion("0.6.5.8")]
+[assembly: AssemblyVersion("0.6.5.9")]
+[assembly: AssemblyFileVersion("0.6.5.9")]
